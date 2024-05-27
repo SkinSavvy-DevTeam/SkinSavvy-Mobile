4953553472fec36a432aef0be10e8de480120611
Putri-R <putrirahmadhani005@gmail.com>
2024-05-27 20:28:21 +0700

    homepage carousel

--- app/src/main/java/com/myapp/skinsavvy/MainActivity.kt
+++ app/src/main/java/com/myapp/skinsavvy/MainActivity.kt
@@ -1,22 +1,51 @@
 package com.myapp.skinsavvy
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.myapp.skinsavvy.adapter.CarouselAdapter
+import com.myapp.skinsavvy.data.pref.DataModel
+import com.myapp.skinsavvy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        val recyclerView: RecyclerView = binding.rvCarouselArticle
-//        val adapter = CarouselAdapter()
-//        recyclerView.adapter = adapter
-//        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        // carousel poster
+        val recyclerViewPoster: RecyclerView = binding.rvCarouselPoster
+        val poster = listOf(
+            DataModel(R.drawable.image_carousel_1),
+            DataModel(R.drawable.image_carousel_2)
+        )
+
+        recyclerViewPoster.adapter = CarouselAdapter(poster, isTextVisible = false)
+        recyclerViewPoster.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        //carousel article
+        val recyclerViewArticle: RecyclerView = binding.rvCarouselArticle
+        val article = listOf(
+            DataModel(R.drawable.image_sample, "Article 1", "deskripsi"),
+            DataModel(R.drawable.image_sample, "Article 2", "deskripsi"),
+            DataModel(R.drawable.image_sample, "Article 3", "deskripsi")
+        )
+
+        recyclerViewArticle.adapter = CarouselAdapter(article, isTextVisible = true)
+        recyclerViewArticle.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        //carousel tips and tricks
+        val recyclerViewTips: RecyclerView = binding.rvCarouselTips
+        val tips = listOf(
+            DataModel(R.drawable.image_sample, "Tips 1", "deskripsi"),
+            DataModel(R.drawable.image_sample, "Tips 2", "deskripsi"),
+            DataModel(R.drawable.image_sample, "Tips 3", "deskripsi")
+        )
+
+        recyclerViewTips.adapter = CarouselAdapter(tips, isTextVisible = true)
+        recyclerViewTips.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
 }
--- app/src/main/java/com/myapp/skinsavvy/adapter/CarouselAdapter.kt
+++ app/src/main/java/com/myapp/skinsavvy/adapter/CarouselAdapter.kt
@@ -1,4 +1,55 @@
 package com.myapp.skinsavvy.adapter
 
-class CarouselAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.myapp.skinsavvy.R
+import com.myapp.skinsavvy.data.pref.DataModel
+import com.myapp.skinsavvy.databinding.CarouselItemLayoutBinding
+import com.myapp.skinsavvy.databinding.CarouselPosterLayoutBinding
+
+
+class CarouselAdapter(private val items: List<DataModel>, private val isTextVisible: Boolean) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return if (isTextVisible) {
+            val binding = CarouselItemLayoutBinding.inflate(inflater, parent, false)
+            ArticleViewHolder(binding)
+        } else {
+            val binding = CarouselPosterLayoutBinding.inflate(inflater, parent, false)
+            PosterViewHolder(binding)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = items[position]
+        when (holder) {
+            is ArticleViewHolder -> {
+                holder.binding.apply {
+                    Glide.with(ivCarouselImage.context)
+                        .load(item.image)
+                        .into(ivCarouselImage)
+                    tvItemName.text = item.title
+                    tvItemDescription.text = item.description
+                }
+            }
+            is PosterViewHolder -> {
+                Glide.with(holder.binding.ivCarouselImage.context)
+                    .load(item.image)
+                    .into(holder.binding.ivCarouselImage)
+            }
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+    class ArticleViewHolder(val binding: CarouselItemLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+
+    class PosterViewHolder(val binding: CarouselPosterLayoutBinding) : RecyclerView.ViewHolder(binding.root)
 }
